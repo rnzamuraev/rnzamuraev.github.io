fb7bb1987fe4b0fcd936d21644e73e0ecc8e1c8a
Ruslan <rnzamuraev@gmail.com>
2023-03-13 19:50:26 +0300

    new projects

--- js/bundle.js
+++ js/bundle.js
@@ -1,14 +1,2 @@
-/******/ (function() { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**************************!*\
-  !*** ./src/js/script.js ***!
-  \**************************/
-window.addEventListener("DOMContentLoaded", () => {
-  // const menu = require("./parts/menu.js");
-  // const skill = require("./parts/skills.js");
-  // const form = require("./parts/form.js");
-});
-
-/******/ })()
-;
+window.addEventListener("DOMContentLoaded",(()=>{}));
 //# sourceMappingURL=bundle.js.map
